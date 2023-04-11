9bbee78312bb64e671091684ea70afef09be2b33
DaveN8 <100126795+DaveN8@users.noreply.github.com>
2023-04-11 08:43:40 +0700

    Section 2

--- 0706012110042-Dave Nathaniel Y-AFL3/ContentView.swift
+++ 0706012110042-Dave Nathaniel Y-AFL3/ContentView.swift
@@ -13,7 +13,9 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundColor(.accentColor)
-            Text("Halo Dek!")
+            Text("Turtle Rock")
+                .font(.title)
+                
         }
         .padding()
     }
