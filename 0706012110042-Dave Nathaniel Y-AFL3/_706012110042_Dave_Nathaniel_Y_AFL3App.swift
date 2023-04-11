f27f1c31e1f37ad66afda4500fd5e17b9eb165ab
DaveN8 <100126795+DaveN8@users.noreply.github.com>
2023-04-11 08:18:39 +0700

    Initial Commit

---
+++ 0706012110042-Dave Nathaniel Y-AFL3/_706012110042_Dave_Nathaniel_Y_AFL3App.swift
@@ -0,0 +1,17 @@
+//
+//  _706012110042_Dave_Nathaniel_Y_AFL3App.swift
+//  0706012110042-Dave Nathaniel Y-AFL3
+//
+//  Created by MacBook Pro on 11/04/23.
+//
+
+import SwiftUI
+
+@main
+struct _706012110042_Dave_Nathaniel_Y_AFL3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
